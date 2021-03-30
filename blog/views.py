2855232f8bf6e5472c045f28c472a411e08475e5
Максим Загорский <you@example.com>
2021-03-30 21:37:54 +0300

    Add forms send email

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
 # def post_list(request):
 #     object_list = Post.published.all()
 #     paginator = Paginator(object_list, 3)
@@ -21,6 +25,18 @@ class PostListView(ListView):
 #     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 # Create your views here.
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id = post_id, status = 'published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+        return render(request, 'blog/post/share.html',
+                      {'post': post, 'form': form})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
