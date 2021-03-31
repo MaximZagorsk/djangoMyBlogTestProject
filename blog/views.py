95d26e7164d12f50bbc29554baa59944ab99611b
Максим Загорский <you@example.com>
2021-03-31 22:18:01 +0300

    Add form comments

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentsForm
 
 
 class PostListView(ListView):
@@ -27,18 +28,37 @@ class PostListView(ListView):
 # Create your views here.
 
 def post_share(request, post_id):
-    post = get_object_or_404(Post, id = post_id, status = 'published')
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['name'], cd['comments'])
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
         return render(request, 'blog/post/share.html',
-                      {'post': post, 'form': form})
+                      {'post': post, 'form': form, 'sent': sent})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentsForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentsForm()
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'new_comments': new_comment,
+                                                     'comment_form': comment_form})
